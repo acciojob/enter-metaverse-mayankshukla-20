4a0b4cecb05bb7742e411c715fa29460be2f899d
Mayank Shukla <117716383+mayankshukla-20@users.noreply.github.com>
2023-12-20 14:54:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const para = document.getElementById("status");
+const button = document.getElementById("enterBtn");
+button.addEventListener("click", () => {
+	para.innertext = "Entered Metaverse"
+	para.style.fontWeight = "bold";
+})
